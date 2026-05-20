83d046c9cdfa739efff19e6118543edf426ed6c8
MaevaPereira <maeva.pereira@le-campus-numerique.fr>
2026-05-20 11:26:32 +0200

    Création du projet Maven

---
+++ project_maven/src/main/java/com/spring_boot/project_maven/ProjectMavenApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.project_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMavenApplication.class, args);
+	}
+
+}
---
+++ project_maven/src/test/java/com/spring_boot/project_maven/ProjectMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.project_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
